3e77e1b95d1614fedeea3bd79e3b9a4933e8176c
theripunbty <168550702+theripunbty@users.noreply.github.com>
2024-08-23 12:11:11 +0530

    Update blockchain-based-library-management-system.sol

--- contracts/blockchain-based-library-management-system.sol
+++ contracts/blockchain-based-library-management-system.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Library {
+    struct Book {
+        uint id;
+        string title;
+        bool isIssued;
+    }
+
+    mapping(uint => Book) public books;
+    uint public bookCount;
+
+    event BookAdded(uint bookId, string title);
+    event BookIssued(uint bookId);
+    event BookReturned(uint bookId);
+
+    function addBook(string memory _title) public {
+        books[++bookCount] = Book(bookCount, _title, false);
+        emit BookAdded(bookCount, _title);
+    }
+
+    function issueBook(uint _bookId) public {
+        require(!books[_bookId].isIssued, "Book already issued");
+        books[_bookId].isIssued = true;
+        emit BookIssued(_bookId);
+    }
+
+    function returnBook(uint _bookId) public {
+        require(books[_bookId].isIssued, "Book not issued");
+        books[_bookId].isIssued = false;
+        emit BookReturned(_bookId);
+    }
+}
 
